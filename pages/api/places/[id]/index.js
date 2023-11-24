bf8c93c5858662b096502f2bcdf35315eba3a74f
VICTOR SANTOS <victor.f089@gmail.com>
2023-11-24 11:55:34 +0100

    almost there

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -1,23 +1,37 @@
 import { db_places } from "../../../../lib/db_places";
 import { db_comments } from "../../../../lib/db_comments";
+import dbConnect from "../../../db/connect";
+import Place from "../../../db/models/Place";
 
-export default function handler(request, response) {
+export default async function handler(request, response) {
+  await dbConnect();
   const { id } = request.query;
 
   if (!id) {
     return;
   }
-
-  const place = db_places.find((place) => place._id.$oid === id);
+  if (request.method === "GET") {
+    const place = await Place.findById(id);
+    if (!place) {
+      return response.status(404).json({ status: "NOT FOUND" });
+    }
+    response.status(200).json(place);
+  }
   const comment = place?.comments;
   const allCommentIds = comment?.map((comment) => comment.$oid) || [];
   const comments = db_comments.filter((comment) =>
     allCommentIds.includes(comment._id.$oid)
   );
-
-  if (!place) {
-    return response.status(404).json({ status: "Not found" });
-  }
-
-  response.status(200).json({ place: place, comments: comments });
 }
+
+// }
+
+//     const place = db_places.find((place) => place._id.$oid === id);
+
+//     if (!place) {
+//       return response.status(404).json({ status: "Not found" });
+//     }
+
+//     response.status(200).json({ place: place, comments: comments });
+//   }
+// }
