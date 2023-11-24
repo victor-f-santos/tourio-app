093228023338ae858aabb095cf15ade7c4296e18
VICTOR SANTOS <victor.f089@gmail.com>
2023-11-24 16:36:17 +0100

    delete done

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -6,7 +6,6 @@ import Place from "../../../../db/models/Place";
 export default async function handler(request, response) {
   await dbConnect();
   const { id } = request.query;
-  console.log(id);
   if (request.method === "GET") {
     const place = await Place.findById(id);
     console.log("placeId: ", place);
@@ -15,33 +14,10 @@ export default async function handler(request, response) {
     }
     response.status(200).json({ place });
   }
+  if (request.method === "DELETE") {
+    const place = await Place.findByIdAndDelete(id);
+    console.log("placeId: ", place);
+
+    return response.status(200).json({ status: "Place deleted" });
+  }
 }
-
-//   if (!id) {
-//     return;
-//   }
-//   if (request.method === "GET") {
-//     const place = await Place.findById(id);
-//     if (!place) {
-//       return response.status(404).json({ status: "NOT FOUND" });
-//     }
-//     response.status(200).json(place);
-//   }
-//   const comment = place?.comments;
-//   const allCommentIds = comment?.map((comment) => comment.$oid) || [];
-//   const comments = db_comments.filter((comment) =>
-//     allCommentIds.includes(comment._id.$oid)
-//   );
-// }
-
-// // }
-
-// //     const place = db_places.find((place) => place._id.$oid === id);
-
-// //     if (!place) {
-// //       return response.status(404).json({ status: "Not found" });
-// //     }
-
-// //     response.status(200).json({ place: place, comments: comments });
-// //   }
-// // }
